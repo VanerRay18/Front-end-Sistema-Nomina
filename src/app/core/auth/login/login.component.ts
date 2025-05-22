a905f6f27dfd68d7c7326e4ed6339b61145eea77
GabrielMH23 <gabrielmh@hotmail.com>
2025-05-22 15:30:50 -0600

    Pantalla login 3

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -29,8 +29,8 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  this.checkTokenExpiration();
-  localStorage.clear();
+    this.checkTokenExpiration();
+    localStorage.clear();
   }
 
   onSubmit() {
@@ -49,50 +49,50 @@ export class LoginComponent implements OnInit {
         user: this.loginForm.value.user,
         password: this.loginForm.value.password
     };
+    
+    // this.authService.authLogg(data).subscribe(
+    //     (response) => {
+    //       console.log(response);
+    //         const token = response.headers.get('Authorization');
+    //         // const tokenExpiration = new Date().getTime() + (2 * 60 * 60 * 1000) + (58 * 60 * 1000); // 2 horas y 58 minutos
+    //         const tokenExpiration = new Date().getTime() + (5000);
+    //         const rolId = response.body.data.roles;
+    //         const userId = response.body.data.userId;
+    //         const path = response.body.data.config.principal;
+    //         const extras = response.body.data.config.extras
+    //         this.PermisosUserService.save(response.body.data.permisos);
+    //         // Guardar el token y otros datos en localStorage
+    //         if (token) {
+    //             localStorage.setItem('userId', userId);
+    //              localStorage.setItem('extras', extras != null? extras : '');
+    //             // localStorage.setItem('eliminar', eliminar);
+    //             // localStorage.setItem('agregar', agregar);
+    //             localStorage.setItem('rolId', rolId);
+    //             localStorage.setItem('token', token);
+    //             localStorage.setItem('tokenExpiration', tokenExpiration.toString());
 
-    this.authService.authLogg(data).subscribe(
-        (response) => {
-            const token = response.headers.get('Authorization');
-            // const tokenExpiration = new Date().getTime() + (2 * 60 * 60 * 1000) + (58 * 60 * 1000); // 2 horas y 58 minutos
-            const tokenExpiration = new Date().getTime() + (5000);
-            const rolId = response.body.data.roles;
-            const userId = response.body.data.userId;
-            const path = response.body.data.config.principal;
-            const extras = response.body.data.config.extras
-            this.PermisosUserService.save(response.body.data.permisos);
-            // Guardar el token y otros datos en localStorage
-            if (token) {
-                localStorage.setItem('userId', userId);
-                 localStorage.setItem('extras', extras != null? extras : '');
-                // localStorage.setItem('eliminar', eliminar);
-                // localStorage.setItem('agregar', agregar);
-                localStorage.setItem('rolId', rolId);
-                localStorage.setItem('token', token);
-                localStorage.setItem('tokenExpiration', tokenExpiration.toString());
+    //             this.router.navigate([path]); // Navegar al nuevo path
+    //         }
 
-                this.router.navigate([path]); // Navegar al nuevo path
-            }
-
-            this.isLoading = false; // Desactivar el loader
-            loginButton.disabled = false; // Habilitar el botón nuevamente
-        },
-        (error) => {
-            Swal.fire({
-              title: error.error.message != null?'Error':'Error con el sistema...',
-                text: error.error.message != null? error.error.message:'Favor de acudir al area de mantenimiento',
-                icon: 'error',
-                confirmButtonText: 'OK'
-            });
-            this.isLoading = false; // Desactivar el loader
-            loginButton.disabled = false; // Habilitar el botón nuevamente
-        }
-    );
+    //         this.isLoading = false; // Desactivar el loader
+    //         loginButton.disabled = false; // Habilitar el botón nuevamente
+    //     },
+    //     (error) => {
+    //         Swal.fire({
+    //           title: error.error.message != null?'Error':'Error con el sistema...',
+    //             text: error.error.message != null? error.error.message:'Favor de acudir al area de mantenimiento',
+    //             icon: 'error',
+    //             confirmButtonText: 'OK'
+    //         });
+    //         this.isLoading = false; // Desactivar el loader
+    //         loginButton.disabled = false; // Habilitar el botón nuevamente
+    //     }
+    // );
 }
 
 
 // Verifica la expiración del token
 checkTokenExpiration() {
-  console.log('hola')
   const tokenExpiration = localStorage.getItem('token');
   if (tokenExpiration) {
     const now = new Date().getTime();
