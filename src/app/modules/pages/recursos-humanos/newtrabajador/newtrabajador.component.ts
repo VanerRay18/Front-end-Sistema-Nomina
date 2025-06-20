0989895b346e80b4b86e3dcf05031bb858680822
GabrielMH23 <gabrielmh@hotmail.com>
2025-06-20 14:58:55 -0600

    gab - nuevo trabajador v2

--- src/app/modules/pages/recursos-humanos/newtrabajador/newtrabajador.component.ts
+++ src/app/modules/pages/recursos-humanos/newtrabajador/newtrabajador.component.ts
@@ -8,17 +8,49 @@ import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms'
 })
 export class NewtrabajadorComponent implements OnInit {
   datosPersonalesForm: FormGroup;
-  nombre: string = '';
+  // nombre: string = '';
 
   constructor(private dpf: FormBuilder, private FormsModule: FormsModule,
   ) {
     this.datosPersonalesForm = this.dpf.group({
-      nombre: ['', [Validators.required , Validators.minLength(15) ]],
+      // nombre: ['', [Validators.required , Validators.minLength(15) ]],
+      foto: [null],
+      nombre: [''],
+      primerApellido: [''],
+      segundoApellido: [''],
+      rfc: [''],
+      curp: [''],
+      catSexoId: [''],
+      catEstadoCivilId: ['']
     });
   }
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
+  }
+
+  guardarDatosPersonales()
+  {
+    const formValue = this.datosPersonalesForm.value;
+
+    // const dto = {
+    //   name,
+    //   firstSurname,
+    //   secondSurname,
+    //   rfc: formValue.rfc,
+    //   curp: formValue.curp,
+    //   nacimiento: new Date(formValue.fechaNacimiento).toISOString(),
+    //   dateStart: new Date(formValue.fechaInicio).toISOString(),
+    //   dateFin: null,
+    //   entrada: formatHorario(formValue.horarioEnt),
+    //   salida: formatHorario(formValue.horarioSal),
+    //   active: true,
+    //   phone: formValue.telefono,
+    //   config: {},
+    //   catJobId: Number(formValue.puestoLaboral),
+    //   catEmploymentId: Number(formValue.tipoContratacion),
+    //   catSeguroId: Number(formValue.tipoSeguro),-+
+    // };
   }
 
 }
